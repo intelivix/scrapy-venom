a123a30840f4490eeb5362ad32ca99f5233ab494
Tiago Lira <tiagoliradsantos@gmail.com>
2016-08-31 10:12:04 -0300

    Adicionando timeout

--- venom/steps.py
+++ venom/steps.py
@@ -8,19 +8,24 @@ from scrapy.http import Request
 
 from venom.tools import is_generator
 from venom.tools import resolve_gen
+from venom.tools import timeout
 
 
 def simple_step(fn):
+
     @functools.wraps(fn)
     def wrapper(response):
+        spider = response.meta['spider']
         kwargs = {
-            'spider': response.meta['spider'],
+            'spider': spider,
             'response': response,
         }
-        for item in fn(**kwargs) or []:
+        decorator = timeout(getattr(spider.timeout, 5))
+        func = decorator(fn)
+        for item in func(**kwargs) or []:
             if isinstance(item, FormRequest) or isinstance(item, Request):
                 meta = item.meta.copy()
-                meta.update({'spider': kwargs['spider']})
+                meta.update({'spider': spider})
                 yield item.replace(meta=meta)
             if is_generator(item) or isinstance(item, types.FunctionType):
                 for item in resolve_gen(item, response=response):
--- venom/tools/__init__.py
+++ venom/tools/__init__.py
@@ -3,3 +3,4 @@
 from .common import *  # noqa
 from .extract import *  # noqa
 from .text import *  # noqa
+from .misc import *  # noqa
